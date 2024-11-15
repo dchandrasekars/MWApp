9cd68b2f98f938bf6b3b5b1b061bcfff80eb9146
dchandrasekars <dchandrasekars@gmail.com>
2024-11-15 22:38:23 +0530

    commit 15-11-24 10-30pm

--- MWApp/mwoffice/views.py
+++ MWApp/mwoffice/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -13,10 +14,10 @@ def home(request):
       user=authenticate(request,username=uname,password=upass)
       if user is not None:
          login(request,user)
-         messages.success(request,"you logged")
+         #messages.success(request,"you logged")
          return redirect('home')
       else:
-         messages.success(request,"not logged")
+         messages.success(request,"Invalid Credentials..try again")
          return redirect('home')
    else:
       return render(request,'home.html',{})
@@ -25,5 +26,24 @@ def login_user(request):
    pass
 
 def logout_user(request):
-   pass
+   logout(request)
+   messages.success(request,"Logged Out..")
+   return redirect('home')
+
+
+def register_user(request):
+   if request.method=='POST':
+      form=SignUpForm(request.POST)
+      if form.is_valid():
+         form.save()
+         #authenticate and login
+         username=form.cleaned_data['username']
+         password=form.cleaned_data['password1']
+         user=authenticate(username=username,password=password)
+         login(request,user)
+         messages.success(request,"Registered Successfully and logged in")
+         return redirect('home')
+   else:
+      form=SignUpForm()
+      return render(request,'register.html',{'form':form})
 
