d154dfd61666cfd35ff028adce1e64b2a24a28ca
dchandrasekars <dchandrasekars@gmail.com>
2024-11-16 22:23:53 +0530

    Revert "commit 16-11-24 11-30am"
    This reverts commit c50e401dc9685859fcd638182872eb0c039ae45c.

--- MWApp/mwoffice/models.py
+++ MWApp/mwoffice/models.py
@@ -1,8 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class CustomUser(AbstractUser):
-    empno = models.CharField(max_length=10, unique=True)  # Employee number
-    ename = models.CharField(max_length=100)             # Employee name
-    section = models.CharField(max_length=50)            # Section or department
-    phone = models.CharField(max_length=15, blank=True, null=True)
+# Create your models here.
--- MWApp/mwoffice/views.py
+++ MWApp/mwoffice/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -31,6 +31,19 @@ def logout_user(request):
    return redirect('home')
 
 
-def register_user(request):   
-      return render(request,'register.html')
+def register_user(request):
+   if request.method=='POST':
+      form=SignUpForm(request.POST)
+      if form.is_valid():
+         form.save()
+         #authenticate and login
+         username=form.cleaned_data['username']
+         password=form.cleaned_data['password1']
+         user=authenticate(username=username,password=password)
+         login(request,user)
+         messages.success(request,"Registered Successfully and logged in")
+         return redirect('home')
+   else:
+      form=SignUpForm()
+      return render(request,'register.html',{'form':form})
 
