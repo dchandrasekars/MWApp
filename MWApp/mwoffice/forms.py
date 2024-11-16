c50e401dc9685859fcd638182872eb0c039ae45c
dchandrasekars <dchandrasekars@gmail.com>
2024-11-16 11:26:17 +0530

    commit 16-11-24 11-30am

--- MWApp/mwoffice/forms.py
+++ MWApp/mwoffice/forms.py
@@ -2,16 +2,3 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-
-class SignUpForm(UserCreationForm):
-    empno=forms.IntegerField()
-    ename=forms.CharField()
-    phone=forms.IntegerField()
-    
-
-class Meta:
-    model=User
-    fields=('username','ename','empno','phoneno','password1','password2')
-
-def __str__(self):
-    return f"{self.username} {self.password1} {self.password2}"
--- MWApp/mwoffice/models.py
+++ MWApp/mwoffice/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    empno = models.CharField(max_length=10, unique=True)  # Employee number
+    ename = models.CharField(max_length=100)             # Employee name
+    section = models.CharField(max_length=50)            # Section or department
+    phone = models.CharField(max_length=15, blank=True, null=True)
